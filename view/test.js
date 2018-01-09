bf1991b9ab3ae29acbe62a63b87f3c7bb0f1345d
chrunlee <chrunlee@foxmail.com>
2018-01-09 14:26:43 +0800

    a

---
+++ view/test.js
@@ -0,0 +1,17 @@
+var url = 'http://588ku.com/';
+var casper = require('casper').create({
+	verbose: true,
+    logLevel: "debug",
+    pageSettings: {
+         loadImages:  true,        
+         loadPlugins: true,    
+         userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2729.4 Safari/537.36'
+    }
+});
+
+casper.start().thenOpen(url,function(){
+	this.wait(15000);
+	this.capture('qianku.png');
+});
+
+casper.run();
